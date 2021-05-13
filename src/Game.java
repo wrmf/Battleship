63e9c4d8ddccfcbbdeba051889d8451da8ca52ab
wrmf <31448920+wrmf@users.noreply.github.com>
2021-05-13 13:54:15 -0500

    Non-gui fully random version works

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,12 @@
+/***
+ * Game class
+ * @author William McGovern-Fagg
+ */
+
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Game {
 	private Player player1;
@@ -21,19 +29,28 @@ public class Game {
 		board1 = player1.placeShips(new Board());
 		board2 = player2.placeShips(new Board());
 		
-		board1.toFile("output1.txt");
-		board2.toFile("output2.txt");
+		File logs1 = new File("battleship_logs1.txt"); 
+		File logs2 = new File("battleship_logs2.txt"); 
+		logs1.delete();
+		logs2.delete();
+		
+		board1.toFile("battleship_logs1.txt");
+		board2.toFile("battleship_logs2.txt");
 		
 		while(!isGameOver()) {
 			board1.fire(player1.getNextMove(board1));
-			board1.toFile("output1.txt");
+			board1.toFile("battleship_logs1.txt");
 			board2.fire(player2.getNextMove(board2));
-			board2.toFile("output2.txt");
+			board2.toFile("battleship_logs2.txt");
 		}
-		board1.toFile("output1.txt");
-		board2.toFile("output2.txt");
+		board1.toFile("battleship_logs1.txt");
+		board2.toFile("battleship_logs2.txt");
 	}
 	
+	/***
+	 * Check if game is over
+	 * @return
+	 */
 	public boolean isGameOver() {
 		if(board1.areAllShipsSunk() || board2.areAllShipsSunk()) {
 			printWinner();
@@ -43,11 +60,36 @@ public class Game {
 		return false;
 	}
 	
+	/***
+	 * Figure out who won
+	 */
 	public void printWinner() {
 		if(board1.areAllShipsSunk()) {
-			System.out.println("Player 1 won");
-		} else {
 			System.out.println("Player 2 won");
+			FileWriter fileWriter;
+			try {
+				fileWriter = new FileWriter("battleship_logs2.txt", true);
+				PrintWriter printWriter = new PrintWriter(fileWriter); //create new printWriter
+				printWriter.println("*************WINNER*************");
+				printWriter.close();
+				fileWriter.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		} else {
+			System.out.println("Player 1 won");
+			FileWriter fileWriter;
+			try {
+				fileWriter = new FileWriter("battleship_logs1.txt", true);
+				PrintWriter printWriter = new PrintWriter(fileWriter); //create new printWriter
+				printWriter.println("*************WINNER*************");
+				printWriter.close();
+				fileWriter.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 	}
 }
--- src/RandomPlayer.java
+++ src/RandomPlayer.java
@@ -6,7 +6,7 @@
 import java.util.Random;
 
 public class RandomPlayer extends Player {
-	
+
 	private int maxTries = 50;
 
 	/***
@@ -24,10 +24,10 @@ public class RandomPlayer extends Player {
 				return location;
 			}
 		}
-		
+
 		return location;
 	}
-	
+
 	/***
 	 * Place all ships on board
 	 * @param board to place ships on
@@ -36,7 +36,7 @@ public class RandomPlayer extends Player {
 	public Board placeShips(Board board) {
 		Location location = new Location();
 		Random random = new Random();
-		
+
 		for(int i = 0; i < board.getNumRows(); i ++) {
 			for(int x = 0; x < board.getNumCols(); x ++) {
 				location.setRow(i);
@@ -44,17 +44,17 @@ public class RandomPlayer extends Player {
 				board.board[i][x] = new Water(location);
 			}
 		}
-		
+
 		board = placeShip(board, random, 5, random.nextBoolean(), location);
 		board = placeShip(board, random, 4, random.nextBoolean(), location);
 		board = placeShip(board, random, 3, random.nextBoolean(), location);
 		board = placeShip(board, random, 3, random.nextBoolean(), location);
 		board = placeShip(board, random, 2, random.nextBoolean(), location);
-		
+
 		return board;
-		
+
 	}
-	
+
 	/***
 	 * Place a ship on board with certain length
 	 * @param board board to change
@@ -66,101 +66,126 @@ public class RandomPlayer extends Player {
 	 */
 	public Board placeShip(Board board, Random random, int length, boolean direction, Location location) {
 		boolean isPlaced = false;
-		
+
 		//Edge cases
 		if(length > 5) {
 			length = 5;
 		} else if(length < 2) {
 			length = 2;
 		}
-		
+
 		if(direction) {
-			location.setRow(random.nextInt(board.getSize()-length));
+			location.setRow(random.nextInt(board.getSize()-length-1));
 			location.setCol(random.nextInt(7));
 			while(!isPlaced) {
-				if(board.isWater(location) && board.isWater(location.getRow()+1, location.getCol()) 
-						&& board.isWater(location.getRow()+2, location.getCol())) {
-					for(int i = 0; i < length; i ++) {
-						if(length == 5) {
-							board.board[i][location.getCol()] = new Carrier(i, location.getCol());
-						} else if(length == 4) {
-							board.board[i][location.getCol()] = new Battleship(i, location.getCol());
-						} else if(length == 3) {
-							board.board[i][location.getCol()] = new Submarine(i, location.getCol());
-						} else if(length == 2) {
-							board.board[i][location.getCol()] = new PatrolBoat(i, location.getCol());
+				//Repeat until ships are placed properly
+				if(length == 5) {
+					if(board.board[location.getRow()][location.getCol()].isWater() && board.board[location.getRow()+1][location.getCol()].isWater() 
+							&& board.board[location.getRow()+1][location.getCol()].isWater() 
+							&& board.board[location.getRow()+2][location.getCol()].isWater() 
+							&& board.board[location.getRow()+3][location.getCol()].isWater() 
+							&& board.board[location.getRow()+4][location.getCol()].isWater()) {
+						for(int i = 0; i < length; i ++) {
+							board.board[location.getRow()+i][location.getCol()] = new Carrier(location.getRow()+i, location.getCol());
 						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setCol(random.nextInt(board.getSize()-length-1));
+						location.setRow(random.nextInt(7));
 					}
-					
-					isPlaced = true;
-				} else { //Reset location
-					location.setRow(random.nextInt(board.getSize()-length));
-					location.setCol(random.nextInt(7));
+				} else if(length == 4) {
+					if(board.isWater(location) && board.isWater(location.getRow()+1, (location.getCol())) 
+							&& board.isWater(location.getRow()+2, location.getCol()) && 
+							board.isWater(location.getRow()+3, location.getCol())) {
+						for(int i = 0; i < length; i ++) {
+							board.board[location.getRow()+i][location.getCol()] = new Battleship(location.getRow()+i, location.getCol());
+						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setRow(random.nextInt(board.getSize()-length-1));
+						location.setCol(random.nextInt(7));
+					}
+				} else if(length == 3) {
+					if(board.isWater(location) && board.isWater(location.getRow()+1, (location.getCol())) 
+							&& board.isWater(location.getRow()+1, location.getCol())) {
+						for(int i = 0; i < length; i ++) {
+							board.board[location.getRow()+i][location.getCol()] = new Submarine(location.getRow()+i, location.getCol());
+						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setRow(random.nextInt(board.getSize()-length-1));
+						location.setCol(random.nextInt(7));
+					}
+				} else if(length == 2) {
+					if(board.isWater(location) && board.isWater(location.getRow()+1, (location.getCol()))) {
+						for(int i = 0; i < length; i ++) {
+							board.board[location.getRow()+i][location.getCol()] = new PatrolBoat(location.getRow()+i, location.getCol());
+						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setRow(random.nextInt(board.getSize()-length-1));
+						location.setCol(random.nextInt(7));
+					}
 				}
 			}
 		} else {
-			location.setCol(random.nextInt(board.getSize()-length));
-			
-			
+			location.setCol(random.nextInt(board.getSize()-length-1));
 			location.setRow(random.nextInt(7));
 			while(!isPlaced) {
-				
+
+				//Repeat until ships are placed properly
 				if(length == 5) {
 					if(board.isWater(location) && board.isWater(location.getRow(), (location.getCol()+1)) 
-							&& board.isWater(location.getRow(), location.getCol()+length+2) && 
-							board.isWater(location.getRow(), location.getCol()+length+3) && 
-							board.isWater(location.getRow(), location.getCol()+length+4)) {
+							&& board.isWater(location.getRow(), location.getCol()+2) && 
+							board.isWater(location.getRow(), location.getCol()+3) && 
+							board.isWater(location.getRow(), location.getCol()+4)) {
 						for(int i = 0; i < length; i ++) {
-							board.board[location.getRow()][i] = new Carrier(i, location.getCol());
+							board.board[location.getRow()][i+location.getCol()] = new Carrier(location.getRow(), location.getCol()+i);
 						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setCol(random.nextInt(board.getSize()-length-1));
+						location.setRow(random.nextInt(7));
 					}
 				} else if(length == 4) {
 					if(board.isWater(location) && board.isWater(location.getRow(), (location.getCol()+1)) 
-							&& board.isWater(location.getRow(), location.getCol()+length+2) && 
-							board.isWater(location.getRow(), location.getCol()+length+3)) {
+							&& board.isWater(location.getRow(), location.getCol()+2) && 
+							board.isWater(location.getRow(), location.getCol()+3)) {
 						for(int i = 0; i < length; i ++) {
-							board.board[location.getRow()][i] = new Battleship(location.getRow(), i);
+							board.board[location.getRow()][i+location.getCol()] = new Battleship(location.getRow(), i+location.getCol());
 						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setCol(random.nextInt(board.getSize()-length-1));
+						location.setRow(random.nextInt(7));
 					}
 				} else if(length == 3) {
 					if(board.isWater(location) && board.isWater(location.getRow(), (location.getCol()+1)) 
-							&& board.isWater(location.getRow(), location.getCol()+length+2)) {
+							&& board.isWater(location.getRow(), location.getCol()+2)) {
 						for(int i = 0; i < length; i ++) {
-							board.board[location.getRow()][i] = new Submarine(location.getRow(), i);
+							board.board[location.getRow()][i+location.getCol()] = new Submarine(location.getRow(), i+location.getCol());
 						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setCol(random.nextInt(board.getSize()-length-1));
+						location.setRow(random.nextInt(7));
 					}
 				} else if(length == 2) {
 					if(board.isWater(location) && board.isWater(location.getRow(), (location.getCol()+1))) {
 						for(int i = 0; i < length; i ++) {
-							board.board[location.getRow()][i] = new PatrolBoat(location.getRow(), i);
+							board.board[location.getRow()][i+location.getCol()] = new PatrolBoat(location.getRow(), i+location.getCol());
 						}
+						isPlaced = true;
+					} else { //Reset location
+						location.setCol(random.nextInt(board.getSize()-length-1));
+						location.setRow(random.nextInt(7));
 					}
-				}
-				
-				
-					for(int i = 0; i < length; i ++) {
-						if(length == 5) {
-							board.board[location.getRow()][i] = new Carrier(i, location.getCol());
-						} else if(length == 4) {
-							board.board[location.getRow()][i] = new Battleship(location.getRow(), i);
-						} else if(length == 3) {
-							board.board[location.getRow()][i] = new Submarine(location.getRow(), i);
-						} else if(length == 2) {
-							board.board[location.getRow()][i] = new PatrolBoat(location.getRow(), i);
-						}
-					}
-					
-					isPlaced = true;
-				} else { //Reset location
-					location.setCol(random.nextInt(board.getSize()-length));
-					location.setRow(random.nextInt(7));
 				}
 			}
 		}
-		
 		return board;
 	}
-	
+
 	/***
 	 * Set max tries for getting next move
 	 * @param maxTries max times to try for a valid move before erroring
@@ -168,7 +193,7 @@ public class RandomPlayer extends Player {
 	public void setMaxTries(int maxTries) {
 		this.maxTries = maxTries;
 	}
-	
+
 	/***
 	 * Get max number of tries for getting next move
 	 * @return maxTries
