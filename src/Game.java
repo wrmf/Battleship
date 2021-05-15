10e8510f275723ec1e3c0f0e6bc2e802c38bfbe3
wrmf <31448920+wrmf@users.noreply.github.com>
2021-05-15 17:44:17 -0500

    Final version

--- src/Game.java
+++ src/Game.java
@@ -9,6 +9,7 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.time.LocalDate;
 
@@ -26,6 +27,8 @@ public class Game {
 	private JButton startButton, fireButton;
 	private ButtonGroup buttonGroup1, buttonGroup2;
 	private JLabel label1, label2;
+	JButton[][] board1buttons;
+	JButton[][] board2buttons;
 	
 	public Game(Player player1, Player player2) {
 		this.player1 = player1;
@@ -35,31 +38,61 @@ public class Game {
 		createGUI();
 	}
 	
+	/***
+	 * Special listener class
+	 * @author william
+	 *
+	 */
 	private class fireListener implements ActionListener {
 		
-		int row;
-		int col;
+		public int row;
+		public int col;
 		Board board;
 		JButton button;
 
-		public fireListener(int row, int col, Board board, JButton button) {
+		public fireListener(int row, int col, JButton button) {
 			this.row = row;
 			this.col = col;
-			this.board = board;
+			this.board = board1;
 			this.button = button;
 		}
 		
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			boolean hit = this.board.fire(new Location(row, col));
-			System.out.println("FIRE!");
-			if(hit) {
-				button.setBackground(Color.RED);
+			
+			if(isGameOver()) {
+				setButtonsEnabled(board1buttons, false);
+				JOptionPane.showMessageDialog(null, "Game over! Player2 won!","Game Over", JOptionPane.NO_OPTION);
 			} else {
-				button.setBackground(Color.WHITE);
+			
+				boolean hit = this.board.fire(new Location(row, col));
+				if(hit) {
+					button.setBackground(Color.RED);
+				} else {
+					button.setBackground(Color.WHITE);
+				}
+				button.setText(board1.board[row][col].draw());
+				button.setEnabled(false);
+				
+				board1.toFile("battleship_logs1.txt");
 			}
-			button.setText(board1.board[row][col].draw());
-			button.setEnabled(false);
+			
+			if(isGameOver()) {
+				setButtonsEnabled(board1buttons, false);
+				JOptionPane.showMessageDialog(null, "Game over! You won!","Game Over", JOptionPane.NO_OPTION);
+			} else {
+				Location loc = new Location();
+				loc = player2.getNextMove(board2, loc);
+				Boolean hit2 = board2.fire(loc);
+				board2buttons[loc.getRow()][loc.getCol()].setText(board2.board[loc.getRow()][loc.getCol()].draw());
+				if(hit2) {
+					board2buttons[loc.getRow()][loc.getCol()].setBackground(Color.RED);
+				} else {
+					board2buttons[loc.getRow()][loc.getCol()].setBackground(Color.WHITE);
+				}
+				
+				board2.toFile("battleship_logs2.txt");
+			}
 		}
 		
 	}
@@ -87,56 +120,48 @@ public class Game {
 		label2 = new JLabel("Opponent's board");
 		
 		
-		JButton[][] board1buttons = new JButton[8][8];
-		JButton[][] board2buttons = new JButton[8][8];
+		board1buttons = new JButton[8][8];
+		board2buttons = new JButton[8][8];
 		
 		frame.setLayout(new BorderLayout());
 		
-//		northPanel.setLayout(new GridLayout(1, 2));
-//		frame.add(northPanel, BorderLayout.NORTH);
-//		northPanel.add(label1);
-//		northPanel.add(label2);
-//		
-//		southPanel.setLayout(new FlowLayout());
-//		southPanel.add(startButton);
-//		frame.add(southPanel, BorderLayout.SOUTH);
+		northPanel.setLayout(new GridLayout(1, 2));
+		frame.add(northPanel, BorderLayout.NORTH);
+		northPanel.add(label1);
+		northPanel.add(label2);
+		
+		southPanel.setLayout(new FlowLayout());
+		southPanel.add(startButton);
+		frame.add(southPanel, BorderLayout.SOUTH);
 
-//		westPanel.setLayout(new GridLayout(8, 8));
-//		frame.add(westPanel, BorderLayout.WEST);
-//		for(int i = 0; i < board1.board.length; i++) {
-//			for(int j = 0; j < board1.board.length; j++) {
-//				board1buttons[i][j] = new JButton(board1.board[i][j].draw());
-//				board1buttons[i][j].addActionListener(new fireListener(i, j, board1, board1buttons[i][j]));
-//				board1buttons[i][j].setBackground(Color.BLUE);
-//				buttonGroup1.add(board1buttons[i][j]);
-//				westPanel.add(board1buttons[i][j]);
-//				board1buttons[i][j].setEnabled(false);
-//			}
-//		}
-
-		frame.add(new Button("North"), BorderLayout.NORTH);
-	    frame.add(new Button("South"), BorderLayout.SOUTH);
-	    frame.add(new Button("East"), BorderLayout.EAST);
-	    frame.add(new Button("West"), BorderLayout.WEST);
-	    frame.add(new Button("Center"), BorderLayout.CENTER);
-	
-//		middlePanel.setLayout(new FlowLayout());
-//		middlePanel.add(new JLabel("YO I AM HERE"));
-//		frame.add(middlePanel, BorderLayout.CENTER);	
+		westPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		westPanel.setLayout(new GridLayout(8, 8));
+		frame.add(westPanel, BorderLayout.WEST);
+		for(int i = 0; i < board1.board.length; i++) {
+			for(int j = 0; j < board1.board.length; j++) {
+				board1buttons[i][j] = new JButton(board1.board[i][j].draw());
+				board1buttons[i][j].addActionListener(new fireListener(i, j, board1buttons[i][j]));
+				board1buttons[i][j].setBackground(Color.BLUE);
+				buttonGroup1.add(board1buttons[i][j]);
+				westPanel.add(board1buttons[i][j]);
+				board1buttons[i][j].setEnabled(false);
+			}
+		}	
 		
-//		frame.add(eastPanel, BorderLayout.EAST);
-//		eastPanel.setLayout(new GridLayout(8, 8));
-//		for(int i = 0; i < board2.board.length; i++) {
-//			for(int j = 0; j < board2.board.length; j++) {
-//				board2buttons[i][j] = new JButton(board1.board[i][j].draw());
-//				board2buttons[i][j].setBackground(Color.BLUE);
-//				buttonGroup2.add(board2buttons[i][j]);
-//				eastPanel.add(board2buttons[i][j]);
-//				board2buttons[i][j].setEnabled(false);
-//			}
-//		}
+		eastPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+		frame.add(eastPanel, BorderLayout.EAST);
+		eastPanel.setLayout(new GridLayout(8, 8));
+		for(int i = 0; i < board2.board.length; i++) {
+			for(int j = 0; j < board2.board.length; j++) {
+				board2buttons[i][j] = new JButton(board1.board[i][j].draw());
+				board2buttons[i][j].setBackground(Color.BLUE);
+				buttonGroup2.add(board2buttons[i][j]);
+				eastPanel.add(board2buttons[i][j]);
+				board2buttons[i][j].setEnabled(false);
+			}
+		}
 		
-		
+			
 		frame.setTitle("Battleship");
 		frame.setSize(1200, 800);
 		frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
@@ -150,49 +175,49 @@ public class Game {
 		
 		frame.setJMenuBar(menuBar);
         frame.setVisible(true);
-//        
-//        quitButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				System.exit(1);
-//			}	
-//		});	
-//        
-//		//Source button opens github
-//		sourceButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				try {
-//					Desktop.getDesktop().browse(new URL("https://github.com/wrmf/Battleship").toURI());
-//				} catch (MalformedURLException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				} catch (IOException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				} catch (URISyntaxException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-//			}
-//		});
-//		
-//		//Source button opens github
-//		startButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				for(int i = 0; i < board1buttons.length; i++) {
-//					for(int j = 0; j < board1buttons[i].length; j++) {
-//						board1buttons[i][j].setEnabled(true);
-//					}
-//				}
-//				play(board1buttons, board2buttons);
-//			}
-//		});
-//		
-//		//Help button gives some amount of help
-//		helpButton.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				JOptionPane.showMessageDialog(null, "Sink all of the enemy's ships to win","Help", JOptionPane.NO_OPTION);
-//			}
-//		});
+        
+        quitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(1);
+			}	
+		});	
+        
+		//Source button opens github
+		sourceButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Desktop.getDesktop().browse(new URL("https://github.com/wrmf/Battleship").toURI());
+				} catch (MalformedURLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (URISyntaxException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+		});
+		
+		//Source button opens github
+		startButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				for(int i = 0; i < board1buttons.length; i++) {
+					for(int j = 0; j < board1buttons[i].length; j++) {
+						board1buttons[i][j].setEnabled(true);
+					}
+				}
+				play(board1buttons, board2buttons);
+			}
+		});
+		
+		//Help button gives some amount of help
+		helpButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, "Sink all of the enemy's ships to win","Help", JOptionPane.NO_OPTION);
+			}
+		});
 		
 	}
 	
@@ -201,27 +226,11 @@ public class Game {
 		File logs2 = new File("battleship_logs2.txt"); 
 		logs1.delete();
 		logs2.delete();
-		
 		board1.toFile("battleship_logs1.txt");
 		board2.toFile("battleship_logs2.txt");
 		
-		Location loc = new Location();
+		setButtonsEnabled(board1buttons, true);
 		
-		while(!isGameOver()) {
-			board1.fire(player1.getNextMove(board1, loc));
-			board1.toFile("battleship_logs1.txt");
-			loc = player2.getNextMove(board2, loc);
-			Boolean hit = board2.fire(loc);
-			board2buttons[loc.getRow()][loc.getCol()].setText(board1.board[loc.getRow()][loc.getCol()].draw());
-			if(hit) {
-				board2buttons[loc.getRow()][loc.getCol()].setBackground(Color.RED);
-			} else {
-				board2buttons[loc.getRow()][loc.getCol()].setBackground(Color.WHITE);
-			}
-			board2.toFile("battleship_logs2.txt");
-		}
-		board1.toFile("battleship_logs1.txt");
-		board2.toFile("battleship_logs2.txt");
 	}
 	
 	/***
@@ -238,11 +247,24 @@ public class Game {
 	}
 	
 	/***
+	 * Set a button array to enabled or disabled
+	 * @param buttonArray
+	 * @param enabled
+	 */
+	public void setButtonsEnabled(JButton[][] buttonArray, boolean enabled) {
+		for(int i = 0; i < buttonArray.length; i++) {
+			for(int j = 0; j < buttonArray.length; j++) {
+				buttonArray[i][j].setEnabled(enabled);
+			}
+		}
+	}
+	
+	/***
 	 * Figure out who won
 	 */
 	public void printWinner() {
 		if(board1.areAllShipsSunk()) {
-			System.out.println("Player 2 won");
+			System.out.println("Player 1 won");
 			FileWriter fileWriter;
 			try {
 				fileWriter = new FileWriter("battleship_logs2.txt", true);
@@ -255,7 +277,7 @@ public class Game {
 				e.printStackTrace();
 			}
 		} else {
-			System.out.println("Player 1 won");
+			System.out.println("Player 2 won");
 			FileWriter fileWriter;
 			try {
 				fileWriter = new FileWriter("battleship_logs1.txt", true);
